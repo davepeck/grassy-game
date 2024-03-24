0a3534719b3c247dac08b7bdc1ae83743e1706c2
Dave <davepeck@gmail.com>
2024-03-24 16:50:51 -0700

    Remove react; it is no more use here.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { createGame } from "./GrassyGame";
 
-// use the modern react 18 api
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement!);
-root.render(<App />);
+createGame(rootElement!);
